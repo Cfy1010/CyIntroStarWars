3828d879ba015c66c87fa17050dcbb127da5fbab
Cyril <cy_one2000@yahoo.fr>
2020-01-20 12:44:55 +0100

    Add css corrections

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
-// Je veux afficher 100 etoiles
-const numbStars = 100;
+// Sets the number of stars we wish to display
+const numStars = 100;
 
-// Pour chaques etoiles je veux afficher des positions aléatoires
-for (let i = 0; i < numbStars; i++){
-    let star = document.createElement('div');
-    star.classname = 'star';
-    var xy = getRandomPositon();
+// For every star we want to display
+for (let i = 0; i < numStars; i++) {
+    let star = document.createElement("div");
+    star.className = "star";
+    var xy = getRandomPosition();
     star.style.top = xy[0] + 'px';
     star.style.left = xy[1] + 'px';
     document.body.append(star);
 }
-    
-// je crée ma fonction de position aléatoire
-function getRandomPositon() {
+
+// Gets random x, y values based on the size of the container
+function getRandomPosition() {
     var y = window.innerWidth;
     var x = window.innerHeight;
-    var randomX = Math.floor(Math.random()*x);
-    var randomY = Math.floor(Math.random()*y);
+    var randomX = Math.floor(Math.random() * x);
+    var randomY = Math.floor(Math.random() * y);
     return [randomX, randomY];
- 
 }
-//    console.log(random);
