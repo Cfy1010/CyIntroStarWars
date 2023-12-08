857c7c3182ca081c4dce7faf9434abd506742db5
Cyril <cy_one2000@yahoo.fr>
2023-12-08 16:46:57 +0100

    add text color & refactor js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 // Sets the number of stars we wish to display
 const numStars = 100;
+const windowHeight = window.innerHeight;
+const windowWidth = window.innerWidth;
+const fragment = document.createDocumentFragment();
 
 // For every star we want to display
 for (let i = 0; i < numStars; i++) {
-    let star = document.createElement("div");
-    star.className = "star";
-    var xy = getRandomPosition();
-    star.style.top = xy[0] + 'px';
-    star.style.left = xy[1] + 'px';
-    document.body.append(star);
+  let star = document.createElement("div");
+  star.className = "star";
+  var xy = getRandomPosition();
+  star.style.top = xy[0] + "px";
+  star.style.left = xy[1] + "px";
+  document.body.append(star);
 }
 
-// Gets random x, y values based on the size of the container
 function getRandomPosition() {
-    var y = window.innerWidth;
-    var x = window.innerHeight;
-    var randomX = Math.floor(Math.random() * x);
-    var randomY = Math.floor(Math.random() * y);
-    return [randomX, randomY];
+  var randomX = Math.floor(Math.random() * window.innerHeight);
+  var randomY = Math.floor(Math.random() * window.innerWidth);
+  return [randomX, randomY];
 }
